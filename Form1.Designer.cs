a7f405d11cf569300ac562ebab53b36d969f05ab
Allan <53708451+FrenchIceCream@users.noreply.github.com>
2023-10-08 11:02:37 +0300

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Graph4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Canvas = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)Canvas).BeginInit();
+            SuspendLayout();
+            // 
+            // Canvas
+            // 
+            Canvas.BackColor = SystemColors.Window;
+            Canvas.Location = new Point(0, 0);
+            Canvas.Name = "Canvas";
+            Canvas.Size = new Size(795, 535);
+            Canvas.TabIndex = 0;
+            Canvas.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(820, 24);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 39);
+            button1.TabIndex = 1;
+            button1.Text = "Задание 1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(820, 82);
+            button2.Name = "button2";
+            button2.Size = new Size(127, 39);
+            button2.TabIndex = 2;
+            button2.Text = "Задание 2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(820, 141);
+            button3.Name = "button3";
+            button3.Size = new Size(127, 39);
+            button3.TabIndex = 3;
+            button3.Text = "Задание 3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(969, 534);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(Canvas);
+            Name = "Form1";
+            Text = "Графика. Лабораторная 4";
+            ((System.ComponentModel.ISupportInitialize)Canvas).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox Canvas;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Graph4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
